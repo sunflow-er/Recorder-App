1818e25efbc1d24cf730ac99b6022f4fff963d3c
sunflow-er <antjd03170@gmail.com>
2024-12-29 01:32:46 +0900

    기본 UI 구성

--- app/src/main/java/com/masonk/recorder/MainActivity.kt
+++ app/src/main/java/com/masonk/recorder/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.masonk.recorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
